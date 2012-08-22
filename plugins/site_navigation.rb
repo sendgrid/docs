3464f98880f1845b6c32de0480ea7ada9a31b1e9
Brandon West <brawest@gmail.com>
2012-08-22 11:57:44 -0600

    tree generation mostly working...

--- plugins/site_navigation.rb
+++ plugins/site_navigation.rb
@@ -1,27 +1,102 @@
+# as we loop through each page
+  # take the last dir in the URL
+  # add the page to that URL's list
+require 'tree'
+require 'set'
+
 module Jekyll
-  class SiteNavigation < Jekyll::Generator
-    safe true
-    priority :lowest
+  # Add accessor for directory
+  class Page
+    attr_reader :dir
+  end
 
-    def generate(site)
-        # First remove all invisible items (default: nil = show in nav)
-        sorted = []
-        site.pages.each do |page|
-           puts page.data.inspect
-           sorted << page if page.data["navigation"]["show"] != false
+  class NavTree < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+    end
+
+    def expand_node(node)
+      if node.name != "/"
+        output = '<li><a href="'+node.content+'">'+ node.content+'</a></li>'
+      else
+        output =''
+      end 
+      puts output
+      
+      
+      
+      if node.has_children?
+        if node.name != "/"
+          output+='<ul class="nav nav-list">'
         end
-
-        # Then sort em according to weight
-        sorted = sorted.sort{ |a,b| a.data["navigation"]["weight"] <=> b.data["navigation"]["weight"] } 
-
-        # Debug info.
-        puts "Sorted resulting navigation:  (use site.config['sorted_navigation']) "
-        sorted.each do |p|
-          puts p.inspect 
+        
+        node.children.each do |child|
+          output += expand_node(child)
         end
+        
+        if node.name != "/"
+          output+='</ul>'
+        end
+      end
+      output
+    end
 
-        # Access this in Liquid using: site.navigation
-        site.config["navigation"] = sorted
+    def render(context)
+      site = context.registers[:site]
+      output=''
+    
+      root_node = Tree::TreeNode.new("/", "Root")
+            
+      site.pages.each do |page|
+        split = (page.dir + page.url).split('/')
+        prev_node = nil 
+        
+        split.each do |chunk|
+          if chunk == "" 
+            next
+          end
+          
+          exists = root_node.find{ |node| node.name == chunk }
+          
+          if exists
+            # node already exists, move along
+            prev_node = exists
+            next
+          end
+
+          node = Tree::TreeNode.new(page.dir + page.url, chunk)
+
+          if prev_node != nil
+            prev_node << node
+            
+            if chunk.index('.') != nil
+              #we've reached a leaf
+              prev_node = nil
+            end
+          else
+            root_node << node
+          end
+
+          prev_node = node
+        end
+        puts root_node.print_tree
+      end
+
+      test = root_node.each { |node| puts node.inspect }
+      
+      #output = expand_node(root_node)
+      #puts output
+
+      #site.pages.each do |page|
+      #  if page.data['title'] && page.data['navigation']['show'] != false
+      #    output += '<li><a href="'+page.dir+page.url+'">'+(page.data['title'] || page.url) +'</a></li>'
+      #  end
+      #end
+
+      output
     end
   end
+
 end
+
+Liquid::Template.register_tag('nav_tree', Jekyll::NavTree)
