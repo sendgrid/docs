06ef1ac38926dd3127d1f5e04d4575ae88733d1d
Brandon West <brawest@gmail.com>
2012-08-22 16:21:30 -0600

    directory tree now working

--- plugins/site_navigation.rb
+++ plugins/site_navigation.rb
@@ -1,55 +1,59 @@
-# as we loop through each page
-  # take the last dir in the URL
-  # add the page to that URL's list
-require 'tree'
-require 'set'
-
 module Jekyll
-  # Add accessor for directory
-  class Page
-    attr_reader :dir
-  end
-
-  class NavTree < Jekyll::Generator
+	# Add accessor for directory
+	class Page
+		attr_reader :dir
+	end
+	
+	class NavTree < Jekyll::Generator
     def generate(site)
-      dirs = ['file1', 'dir1/file2', 'dir1/subdir1/file3',  'dir1/subdir1/file5']
+      dirs = Array.new
       tree = {}
 
+      site.pages.each do |page|
+        dirs.push(page.dir + page.url)
+      end
+
       dirs.each do |path|
-        current  = tree
+        current	 = tree
         path.split("/").inject("") do |sub_path,dir|
           sub_path = File.join(sub_path, dir)
           current[sub_path] ||= {}
-          current  = current[sub_path]
+          current	 = current[sub_path]
           sub_path
         end
       end
 
-      def print_tree(prefix, node)
-        puts "#{prefix}<ul>"
-        node.each_pair do |path, subtree| 
-          puts "#{prefix}  <li>[#{path[1..-1]}] #{File.basename(path)}</li>"    
-          print_tree(prefix + "  ", subtree) unless subtree.empty?
-        end
-        puts "#{prefix}</ul>"
+      site.config["navigation"] = files_first_traverse "", tree
+    end
+	
+    def print_tree(prefix, node)
+      puts "#{prefix}<ul class=\"nav nav-list\">"
+      node.each_pair do |path, subtree| 
+        puts "#{prefix}	 <li>[#{path[1..-1]}] #{File.basename(path)}</li>"		
+        print_tree(prefix + "	 ", subtree) unless subtree.empty?
       end
-
-
+      puts "#{prefix}</ul>"
+    end
+	
+	  
+    def files_first_traverse(prefix, node = {})
+      output = ""
+      output += "#{prefix}<ul class=\"nav nav-list\">" 
+      node_list = node.sort
       
-      def files_first_traverse(prefix, node = {})
-        puts "#{prefix}<ul>" 
-        node_list = node.sort
-        node_list.each do |base, subtree|
-          puts "#{prefix}  <li>#{base}</li>" if subtree.empty?
-        end
-        node_list.each do |base, subtree|
-          next if subtree.empty?
-          puts "#{prefix}  <li>#{base}</li>"
-          files_first_traverse(prefix + '  ', subtree)
-        end
-        puts '#{prefix}</ul>'
+      node_list.each do |base, subtree|
+        output += "#{prefix}	 <li>#{base}</li>" if subtree.empty?
       end
       
-      files_first_traverse "", tree
-  end
+      node_list.each do |base, subtree|
+        next if subtree.empty?
+          output += "#{prefix}	 <li>#{base}</li>"
+          output += files_first_traverse(prefix + '	 ', subtree)
+        end
+      
+        output += "#{prefix} </ul>"
+        output
+      end
+
+    end
 end
