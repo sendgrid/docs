36d120074a34caa9611e57b49f9e3a51db7267e6
Brandon West <brawest@gmail.com>
2012-08-22 22:43:56 -0600

    nav generator now respects title metadata and auto-adds link to ./index.html for directory-level nodes

--- plugins/site_navigation.rb
+++ plugins/site_navigation.rb
@@ -6,7 +6,7 @@ module Jekyll
 	
 	class NavTree < Jekyll::Generator
     def generate(site)
-      dirs = {}
+      @dirs = {}
       tree = {}
 
       site.pages.each do |page|
@@ -14,17 +14,17 @@ module Jekyll
         if page.data["navigation"]["show"] != false
           relative = page.dir[1..-1] ||""
           path = relative + page.url
-          puts path
           path = path.index('/')==0 ? path[1..-1] : path
           puts path
-          dirs[path] = page.data
+          @dirs[path] = page.data
         end
       end
-      dirs.each do |path, data|
+      @dirs.each do |path, data|
         current	 = tree
         path.split("/").inject("") do |sub_path,dir|
-          puts sub_path
           sub_path = File.join(sub_path, dir)
+          puts sub_path
+          
           current[sub_path] ||= {}
           current	 = current[sub_path]
           sub_path
@@ -41,13 +41,22 @@ module Jekyll
       
       node_list.each do |base, subtree|
           name = base[1..-1]
-          output += "#{prefix}	 <li><a href=>#{name}</a></li>" if subtree.empty?
+          if name.index('.') != nil
+            name = @dirs[name]["title"] || name
+          end
+          output += "#{prefix}	 <li><a href=\"#{base}\">#{name}</a></li>" if subtree.empty?
       end
       
       node_list.each do |base, subtree|
         next if subtree.empty?
+          href = base
           name = base[1..-1]
-          output += "#{prefix}	 <li><a href=>#{name}</a></li>"
+          if name.index('.') != nil
+            name = @dirs[name]["title"] || name
+          else
+            href = href + "/index.html"
+          end
+          output += "#{prefix}	 <li><a href=\"#{href}\">#{name}</a></li>"
           output += files_first_traverse(prefix + '	 ', subtree)
         end
       
