439b1341f30eca18178a191b97ccc4872202f9e5
Jarod Stewart <stewartjarod@gmail.com>
2013-04-24 08:45:05 -0600

    Initial VidGrid
    Initial layout, only has one video added, will be completing all other
    video pages today.
    
    Need to merge changes from Monday that went out.

--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -67,3 +67,10 @@ $(function() {
 	    });
 	}
 });
+
+// Vid Grid Menu
+$(document).ready(function(){  
+	$('.accordion').on('show hide', function(e) {
+	    $(e.target).siblings('.accordion-heading').find('.accordion-toggle i').toggleClass('icon-counter-active icon-counter-inactive', 200);
+	});
+});
