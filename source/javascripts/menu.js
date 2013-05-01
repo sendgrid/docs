5f96a554e79f8e839e619c6de2922246b9a80b3e
Jarod Stewart <stewartjarod@gmail.com>
2013-05-01 15:06:47 -0600

    VidGrid Clean Up
    Separated vidgrid css and js
    Removed from Head.html

--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -33,10 +33,3 @@ $(function() {
 		$('.collapsibleListOpen, .collapsibleListClosed').click(saveTreeState);	
 	}
 });
-
-// Vid Grid Menu
-$(document).ready(function(){  
-	$('.accordion').on('show hide', function(e) {
-	    $(e.target).siblings('.accordion-heading').find('.accordion-toggle i').toggleClass('icon-counter-active icon-counter-inactive', 200);
-	});
-});
