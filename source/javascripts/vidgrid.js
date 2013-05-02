8e3e1371b2b64de037db49cf314322fd74640c6b
Brandon West <brawest@gmail.com>
2013-05-02 14:18:33 -0600

    remove vidgrid specific js, fix navigation to highlight vidgrid for all pages

--- source/javascripts/vidgrid.js
+++
@@ -1,6 +0,0 @@
-// Vid Grid Menu
-$(document).ready(function(){  
-	$('.accordion').on('show hide', function(e) {
-	    $(e.target).siblings('.accordion-heading').find('.accordion-toggle i').toggleClass('icon-counter-active icon-counter-inactive', 200);
-	});
-});
