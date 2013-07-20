ac9ea05de2e4edb845fd2b5c78d3b0aa658e329f
Brandon West <brawest@gmail.com>
2013-07-19 22:34:23 -0600

    fix deploy?

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 require 's3-static-site'
-set :application, 'SendGrid-Docs'
+set :application, ""
+set :deploy_to, ""
 set :bucket, ENV["BUCKET"]
 set :access_key_id, ENV["ACCESS_KEY"]
 set :secret_access_key, ENV["SECRET_KEY"]
