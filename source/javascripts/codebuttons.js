94d4b27df10cdce1e68b1276ca702b38756a6b5d
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2013-08-30 12:23:51 -0700

    New expansion.

--- source/javascripts/codebuttons.js
+++ source/javascripts/codebuttons.js
@@ -17,18 +17,66 @@ $(function() {
     ZeroClipboard.setDefaults({ moviePath: path, hoverClass: 'hover' });
 
     // Initialize all zClipboards
-    $(".copycode").each(function () {
+    glueClipboards();
+  }else{
+    $("body").addClass("no-flash");
+  }
+
+  $(".expandcode").click(function() {
+    if($("#overlay").length){
+      $("#overlay").trigger("close");
+    }else{
+      var codeId = $(this).attr("id").replace('expand_','');
+      var codeblockWrapper = $('#wrapper_' + codeId);
+      codeblockWrapper.addClass('expanded');
+
+      var overlay = $('<div id="overlay"></div>');
+      overlay.appendTo(document.body);
+
+      codeblockWrapper.children().appendTo(overlay);
+
+      $(this).html('<i class="icon-fullscreen"> </i> Close');
+
+      //prevent the body from scrolling
+      $(document.body).css("overflow", "hidden");
+
+      overlay.on("close", function() {
+        var overlay = $('#overlay');
+
+        var codeId = overlay.children(".code").attr("id").replace('code_','');
+        var codeblockWrapper = $('#wrapper_' + codeId);
+
+        $("#overlay .expandcode").html('<i class="icon-fullscreen"> </i> Fullscreen');
+        
+        overlay.children().appendTo(codeblockWrapper);
+        $(document.body).css("overflow", "auto");
+        overlay.remove();
+      });
+
+      overlay.on("click", function(e) {
+        if(e.target == $(this)[0]){
+          $(this).trigger("close");
+        }
+      });
+    }
+    ZeroClipboard.destroy();
+    glueClipboards();
+  });
+});
+
+function glueClipboards () {
+      $(".copycode").each(function () {
 
       var zclip = new ZeroClipboard($(this));
      
       zclip.on('dataRequested', function ( client, args ) {
         // Find the hash of the code
-        var codeIdentifier = $(this).attr("id").replace('copy_','');
+        var codeId = $(this).attr("id").replace('copy_','');
         // Get the text of the requested copy
-        var text = $('#code_' + codeIdentifier + ' .code').text();
+        var text = $('#code_' + codeId + ' .code').text();
 
         // Get rid of the final \n as that's not really expected by users
-        text.replace(/\n?$/,'');
+        text.replace(/\n$/,'');
 
         client.setText( text );
       });
@@ -37,49 +85,4 @@ $(function() {
         $(this).html('<i class="icon-check"> </i> Copied');
       });
     });
-  }else{
-    $("body").addClass("no-flash");
-  }
-
-  $(".expandcode").click(function() {
-    var codeId = $(this).attr("id").replace('expand_','');
-    var codeblock = $('#wrapper_' + codeId + " figure.code")
-    codeblock.addClass('expanded');
-
-    var overlay = $('<div id="overlay"></div>');
-    overlay.appendTo(document.body);
-
-    //prevent the body from scrolling
-    $(document.body).css("overflow", "hidden");
-
-    innerblock = codeblock.find('.highlight');
-    innerblock.css("overflow", "scroll"); 
-    innerblock.css("height", $(window).height() - 100 + "px");
-    innerblock.css("width", $(window).width() - 80 + "px");
-    
-    //move all of this into a css class and toggle it
-    //append a button that closes the window
-    button = $('<a class="close-btn btn btn-large btn-danger"><i class="icon-remove"></i> Close</a>');
-    button.css("float","right");
-    button.css("position","fixed");
-    button.css("top","20px");
-    button.css("right","40px");
-    innerblock.append(button);
-
-    button.click(function() {
-      $(document.body).css("overflow", "auto");
-      $('#overlay').remove();
-      
-      var codeblock = $(this).closest("figure.code")
-      codeblock.removeClass('expanded');
-      codeblock.css("width","auto");
-
-      innerblock = codeblock.find('.highlight');
-      innerblock.css("width","auto");
-      innerblock.css("height","auto");
-      innerblock.css("overflow", "auto"); 
-      
-      $(this).remove();
-    });
-  });
-});
+}
