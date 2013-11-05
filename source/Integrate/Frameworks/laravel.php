5e7b6fad9adb4ead26766895d6db861702c9de36
Will Smidlein <Will@WillSmidlein.com>
2013-11-04 20:32:47 -0500

    Some better formatting and some demo code

--- source/Integrate/Frameworks/laravel.php
+++ source/Integrate/Frameworks/laravel.php
@@ -9,7 +9,7 @@ navigation:
 <p>Laravel comes with an email sending library built in. See more information on how to <a href="http://four.laravel.com/docs/mail">use Laravel with SendGrid</a>.
 </p>
 
-In <strong>app/config/mail.php</strong> you need to configure these settings:
+In {% codeblock %}app/config/mail.php{% endcodeblock %} you need to configure these settings:
 {% codeblock lang:php %}
 <?php
 
@@ -22,6 +22,19 @@ return array(
 	'username' => 'sendgrid_username',
 	'password' => 'sendgrid_password',
 );
+
+?>
+{% endcodeblock %}
+
+<p>You can use Laravel's Mail class just like you normally would, but all email will be sent through SendGrid!</p>
+
+{% codeblock lang:php %}
+<?php
+
+Mail::send('emails.demo', $data, function($message)
+{
+    $message->to('jane@example.com', 'Jane Doe')->subject('This is a demo!');
+});
 
 ?>
 {% endcodeblock %}
