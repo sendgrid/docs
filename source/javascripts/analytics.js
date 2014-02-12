8ff09af594b8a30dc10b4aac8234c71df04d290c
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-02-12 13:30:52 -0800

    switched out analytics
    Switched out Google Analytics for Google Tag Manager, as SendGrid is
    now adopting Tag Manger as the way to put any tags on our site.
    
    Furthermore, patched analytics.js code as the default code to track
    events, while keeping ga.js fallbacks

--- source/javascripts/analytics.js
+++ source/javascripts/analytics.js
@@ -1,4 +1,27 @@
-$(function () {
+jQuery(function ($) {
+
+	function pushEvent (eventInfo) {
+		var gauEventInfo = {
+			'hitType': 'event',
+			'eventCategory': eventInfo[1],
+			'eventAction': eventInfo[2]
+		};
+		if(eventInfo[3]){
+			gauEventInfo['eventLabel'] = eventInfo[3];
+		}
+		if(eventInfo[4]){
+			gauEventInfo['eventValue'] = eventInfo[4];
+		}
+		if(eventInfo[5]){
+			gauEventInfo['nonInteraction'] = Number(eventInfo[4]);
+		}
+
+		if(typeof ga === "function") {
+			ga('send', gauEventInfo);
+		} else if (_gaq) {
+			_gaq.push(eventInfo);
+		}
+	}
 
 	$.expr[':'].external = function(obj){
 		return !obj.href.match(/^mailto:/) && !obj.href.match(/^#:/) && (obj.hostname.replace(/^www\./i, '') != document.location.hostname.replace(/^www\./i, ''));
@@ -9,14 +32,14 @@ $(function () {
 		var eventLabel = $(this).attr("href");
 
 		var eventInfo = ['_trackEvent', 'Link', 'Outbound', eventLabel, null, true];
-		_gaq.push(eventInfo);
+		pushEvent(eventInfo);
 	});
 
 	// Track any email sends
 	$('a[href^="mailto:"]').on("click keypress", function (e) {
 		var eventLabel = $(this).attr("href").substring(7);
 		var eventInfo = ['_trackEvent', 'Link', 'Email', eventLabel];
-		_gaq.push(eventInfo);
+		pushEvent(eventInfo);
 	});
 
 });
