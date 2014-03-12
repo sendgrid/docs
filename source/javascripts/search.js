544f6fcf6df65e090ab8dcfa4f522c91845f8429
Brandon West <brawest@gmail.com>
2014-03-12 10:57:28 -0600

    disable search analytics until fixed

--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -32,7 +32,7 @@
   function search(query) {
   	var relativeRoot = $('.nav-link > a')[0].pathname.replace('/index.html','');
   	var simulatedSearchUrl = relativeRoot + '/search.html?q=' + encodeURIComponent(query);
-  	_gaq.push(['_set','page',simulatedSearchUrl]);
+  	//_gaq.push(['_set','page',simulatedSearchUrl]);
   	
     var result = $.getJSON('http://agupam.api.indexden.com/v1/indexes/docs/search?q=' + encodeURIComponent(query) + '&fetch=title&snippet=text&len=500&callback=?', function(data) {
         $('div#results').empty();
