dbfc22384ec403849f7a1205ad7afd4e42a1f022
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-03-18 17:16:14 -0700

    updated ga.js to analytics.js
    As we moved to the new tag manager we moved to the new Google Analytics
    which has a different javascript API.
    
    Several issues were caused by this change while not adequately
    counteracting them with code, this fixes them.
    
    Closes #325! Closes #327!

--- source/javascripts/analytics.js
+++ source/javascripts/analytics.js
@@ -1,27 +1,28 @@
-jQuery(function ($) {
 
-	function pushEvent (eventInfo) {
-		var gauEventInfo = {
-			'hitType': 'event',
-			'eventCategory': eventInfo[1],
-			'eventAction': eventInfo[2]
-		};
-		if(eventInfo[3]){
-			gauEventInfo['eventLabel'] = eventInfo[3];
-		}
-		if(eventInfo[4]){
-			gauEventInfo['eventValue'] = eventInfo[4];
-		}
-		if(eventInfo[5]){
-			gauEventInfo['nonInteraction'] = Number(eventInfo[4]);
-		}
-
-		if(typeof ga === "function") {
-			ga('send', gauEventInfo);
-		} else if (_gaq) {
-			_gaq.push(eventInfo);
-		}
+function gaPushEvent (eventInfo) {
+	var gauEventInfo = {
+		'hitType': 'event',
+		'eventCategory': eventInfo[1],
+		'eventAction': eventInfo[2]
+	};
+	if(eventInfo[3]){
+		gauEventInfo['eventLabel'] = eventInfo[3];
 	}
+	if(eventInfo[4]){
+		gauEventInfo['eventValue'] = eventInfo[4];
+	}
+	if(eventInfo[5]){
+		gauEventInfo['nonInteraction'] = Number(eventInfo[4]);
+	}
+
+	if(typeof ga === "function") {
+		ga('send', gauEventInfo);
+	} else if (_gaq) {
+		_gaq.push(eventInfo);
+	}
+}
+
+jQuery(function ($) {
 
 	$.expr[':'].external = function(obj){
 		return !obj.href.match(/^mailto:/) && !obj.href.match(/^#:/) && (obj.hostname.replace(/^www\./i, '') != document.location.hostname.replace(/^www\./i, ''));
@@ -32,14 +33,14 @@ jQuery(function ($) {
 		var eventLabel = $(this).attr("href");
 
 		var eventInfo = ['_trackEvent', 'Link', 'Outbound', eventLabel, null, true];
-		pushEvent(eventInfo);
+		gaPushEvent(eventInfo);
 	});
 
 	// Track any email sends
 	$('a[href^="mailto:"]').on("click keypress", function (e) {
 		var eventLabel = $(this).attr("href").substring(7);
 		var eventInfo = ['_trackEvent', 'Link', 'Email', eventLabel];
-		pushEvent(eventInfo);
+		gaPushEvent(eventInfo);
 	});
 
 });
