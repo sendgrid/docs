69e6cdf799750ed4bdb512de8527f7bae7862c48
Brandon West <brawest@gmail.com>
2014-03-21 09:29:50 -0600

    deleted old assets folders, converted css to jekyll_asset_pipeline

--- source/javascripts/fonts.js
+++
@@ -1,8 +0,0 @@
-var MTIProjectId='0e71051d-80ba-489d-a6f6-b2adbda0057b';
-(function() {
-  var mtiTracking = document.createElement('script');
-  mtiTracking.type='text/javascript';
-  mtiTracking.async='true';
-  mtiTracking.src=('https:'==document.location.protocol?'https:':'http:')+'//fast.fonts.net/t/trackingCode.js';
-  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
-})();
