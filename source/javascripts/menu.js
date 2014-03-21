bfe8667cf285bce08f768fa3261471e6c219bc4a
Brandon West <brawest@gmail.com>
2014-03-20 19:22:16 -0600

    remove saving of nav tree state

--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -1,36 +1,6 @@
-var saveTreeState = function(){
-	parent_nodes = $('#nav-menu  .parent'); 
-
-	parent_nodes.each(function(i){		
-		open = $(this).hasClass('collapsibleListOpen');
-		$.jStorage.set($(this).attr('id'), open)
-	}); 
-}
-
-var loadTreeState = function(){
-	parent_nodes = $('#nav-menu .parent'); 
-
-	parent_nodes.each(function(i){		
-		node = $(this);
-		open = $.jStorage.get(node.attr('id'))
-
-		// the toggle flips between states on load, so need to set them to the opposite
-		if (open){ 
-			node.addClass('collapsibleListClosed');
-		}
-		else {
-			node.addClass('collapsibleListOpen');
-		}
-		
-	});
-}
-
 $(function() {
-	loadTreeState();
 	if($('#nav-menu').length) {
 		CollapsibleLists.applyTo(document.getElementById('nav-menu'));
-	
-		$('.collapsibleListOpen, .collapsibleListClosed').click(saveTreeState);	
 	}
 
 	$.ajax({
