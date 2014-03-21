5e06ed83030f0aeca0f4bd22a860d8c90fc59bd0
Brandon West <brawest@gmail.com>
2014-03-21 09:59:37 -0600

    convert js to jekyll-asset-pipeline, add less preprocessing

--- plugins/jekyll_asset_pipeline.rb
+++ plugins/jekyll_asset_pipeline.rb
@@ -1,1 +1,16 @@
 require 'jekyll_asset_pipeline'
+
+module JekyllAssetPipeline
+  class LessConverter < JekyllAssetPipeline::Converter
+    require 'less'
+
+    def self.filetype
+      '.less'
+    end
+
+    def convert
+      parser = Less::Parser.new
+      return parser.parse(@content).to_css
+    end
+  end
+end
