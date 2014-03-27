3dc6fb964c6e7a4313e527db0457ea5b31b16701
Brandon West <brawest@gmail.com>
2014-03-27 15:42:59 -0600

    rig things up to use ajax

--- source/_assets/javascripts/livedocs.js
+++ source/_assets/javascripts/livedocs.js
@@ -2,4 +2,23 @@ $(function () {
   $('.tryit').click(function(){ 
     $(this).next('form').show();
   });
+
+  $('.live-doc form').submit(function(e){
+    e.preventDefault();
+
+    url = $(this).parent().find('.url').val();
+    method = $(this).parent().find('.method').val();
+
+    console.log(url);
+    console.log(method);
+
+    $.ajax({
+      type: method,
+      url: url,
+      data: $(this).serialize(),
+    })
+    .done(function(response) {
+      alert(response);
+    });
+  });
 });
