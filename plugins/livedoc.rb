7308659fcdca2beba1e3f8a93416a959ed559fa4
Brandon West <brawest@gmail.com>
2014-03-27 13:37:32 -0600

    move markup to a plugin instead of client code

---
+++ plugins/livedoc.rb
@@ -0,0 +1,47 @@
+require 'rack'
+
+module Jekyll
+  class LiveDocTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      parameters = markup.split(" ")
+      @method = parameters[0].upcase
+      @url = parameters[1]
+      data = parameters[2]
+
+      #parse the querystring data into a hash
+      @params = Rack::Utils.parse_nested_query(data)
+      super
+    end
+
+    def render(context)
+      #wondering what this syntax is? google "here document"
+      inputs = ""
+      @params.each do |key, value|        
+        if value.is_a?(Array)
+          value = "[" + value.join(",") + "]"
+        end
+
+        if value.nil? 
+          value = ""
+        end
+
+        inputs = inputs + '<div class="form-group"><label><span>' + key + '</span>'
+        inputs = inputs + '<input type="text" class="form-control" name="' + key + '" + value="' + value + '">'
+        inputs = inputs + '</input></label></div>'
+      end
+      output=<<-HTML
+      <div class="live-doc">
+        <button class="btn btn-primary tryit">Try It</button>
+        <form role="form" class="well">
+          #{inputs}
+          <button type="input" class="btn btn-default">Make Request</button>
+        </form>
+      </div>
+      HTML
+
+      output
+    end
+  end
+end
+
+Liquid::Template.register_tag('livedoc', Jekyll::LiveDocTag)
