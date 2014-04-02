091fcca12445ef25a57bfb5d5cb9cccc23fe51b7
Brandon West <brawest@gmail.com>
2014-04-02 09:06:04 -0600

    improve how YAML config is changed during build -- see set_prod_config.rb

---
+++ set_prod_config.rb
@@ -0,0 +1,8 @@
+require 'yaml'
+  config = YAML.load_file('_config.yml')
+  config['root'] = '/docs'
+  config['indextank'] = 'true'
+
+  File.open('_config.yml','w') do |f| 
+     f.write config.to_yaml
+end
