a82dd5008f5f3ef5754f490aa0dca3c4fd790416
Brandon West <brawest@gmail.com>
2014-04-02 12:26:32 -0600

    fix indextank flag

--- set_prod_config.rb
+++ set_prod_config.rb
@@ -1,7 +1,7 @@
 require 'yaml'
   config = YAML.load_file('_config.yml')
   config['root'] = '/docs'
-  config['indextank'] = 'true'
+  config['indextank'] = true
 
   File.open('_config.yml','w') do |f| 
      f.write config.to_yaml
