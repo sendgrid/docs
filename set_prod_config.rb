e7e441f6823c8926c0b1bbabd3e73f57217685e7
Brandon West <brawest@gmail.com>
2014-04-02 13:01:20 -0600

    add indextank env vars

--- set_prod_config.rb
+++ set_prod_config.rb
@@ -2,6 +2,8 @@ require 'yaml'
   config = YAML.load_file('_config.yml')
   config['root'] = 'http://sendgrid.com/docs'
   config['indextank'] = true
+  config['indextank_index'] = ENV["INDEXTANK_INDEX"]
+  config['indextank_api_url'] = ENV["INDEXTANK_API_URL"]
 
   File.open('_config.yml','w') do |f| 
      f.write config.to_yaml
