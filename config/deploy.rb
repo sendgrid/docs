929c19260b84c40b505356bd293e38b1c6a589a0
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-04-29 12:28:21 +0100

    added checks so PRs to master don't get deployed
    closes #367

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,11 @@ set :application, ""
 set :deploy_to, ""
 
 if ENV["TRAVIS_BRANCH"] == "master"
-  set :bucket, ENV["PROD_BUCKET"]
-  set :access_key_id, ENV["PROD_ACCESS_KEY"]
-  set :secret_access_key, ENV["PROD_SECRET_KEY"]
+  if ENV["TRAVIS_PULL_REQUEST"] == false
+    set :bucket, ENV["PROD_BUCKET"]
+    set :access_key_id, ENV["PROD_ACCESS_KEY"]
+    set :secret_access_key, ENV["PROD_SECRET_KEY"]
+  end
 else
   set :bucket, ENV["BUCKET"]
   set :access_key_id, ENV["ACCESS_KEY"]
