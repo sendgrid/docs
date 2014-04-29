2f358e3bf15a94b8d7f5411d3b28cc7e18447933
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-04-29 13:13:11 +0100

    updated no-deploy check

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,9 @@ set :application, ""
 set :deploy_to, ""
 
 if ENV["TRAVIS_BRANCH"] == "master"
-  if ENV["TRAVIS_PULL_REQUEST"] == false
-    set :bucket, ENV["PROD_BUCKET"]
-    set :access_key_id, ENV["PROD_ACCESS_KEY"]
-    set :secret_access_key, ENV["PROD_SECRET_KEY"]
-  end
+  set :bucket, ENV["PROD_BUCKET"]
+  set :access_key_id, ENV["PROD_ACCESS_KEY"]
+  set :secret_access_key, ENV["PROD_SECRET_KEY"]
 else
   set :bucket, ENV["BUCKET"]
   set :access_key_id, ENV["ACCESS_KEY"]
