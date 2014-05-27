a339094d9a78b52996c86d092ba74306924f9d2c
Brandon West <brawest@gmail.com>
2014-05-27 09:38:16 -0600

    revert unnecessary change

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,11 +6,9 @@ if ENV["TRAVIS_BRANCH"] == "master"
   set :bucket, ENV["PROD_BUCKET"]
   set :access_key_id, ENV["PROD_ACCESS_KEY"]
   set :secret_access_key, ENV["PROD_SECRET_KEY"]
-  set :cloudfront_distro_id, ENV["PROD_CLOUDFRONT_ID"]
 else
   set :bucket, ENV["BUCKET"]
   set :access_key_id, ENV["ACCESS_KEY"]
   set :secret_access_key, ENV["SECRET_KEY"]
-  set :cloudfront_distro_id, ENV["DEV_CLOUDFRONT_ID"]
 end
 
