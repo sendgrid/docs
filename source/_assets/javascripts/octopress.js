7b12810896a03057ed1da42265061c2fd5ae3c79
Brandon West <brawest@gmail.com>
2014-05-28 19:07:10 -0600

    code workshop!

--- source/_assets/javascripts/octopress.js
+++ source/_assets/javascripts/octopress.js
@@ -12,31 +12,6 @@ function getNav() {
   });
 }
 
-function addSidebarToggler() {
-  if(!$('body').hasClass('sidebar-footer')) {
-    $('#content').append('<span class="toggle-sidebar"></span>');
-    $('.toggle-sidebar').bind('click', function(e) {
-      e.preventDefault();
-      if ($('body').hasClass('collapse-sidebar')) {
-        $('body').removeClass('collapse-sidebar');
-      } else {
-        $('body').addClass('collapse-sidebar');
-      }
-    });
-  }
-  var sections = $('aside.sidebar > section');
-  if (sections.length > 1) {
-    sections.each(function(index, section){
-      if ((sections.length >= 3) && index % 3 === 0) {
-        $(section).addClass("first");
-      }
-      var count = ((index +1) % 2) ? "odd" : "even";
-      $(section).addClass(count);
-    });
-  }
-  if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
-}
-
 function testFeatures() {
   var features = ['maskImage'];
   $(features).map(function(i, feature) {
@@ -115,7 +90,6 @@ $('document').ready(function() {
   flashVideoFallback();
   addCodeLineNumbers();
   getNav();
-  addSidebarToggler();
 });
 
 // iOS scaling bug fix
