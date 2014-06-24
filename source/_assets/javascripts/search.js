2934acf84a5ff6dacd0f4fc8f47e4b40f650db69
Brandon West <brawest@gmail.com>
2014-06-24 08:31:22 -0600

    add support knowledge base search and change ajax requests to use promises and display error messages on failure

--- source/_assets/javascripts/search.js
+++ source/_assets/javascripts/search.js
@@ -1,7 +1,5 @@
-//just a quick example, in no way pretty code.
 //if we load the page and there's a query specified (from the header search input), execute that query immediately
-
-  $(document).ready(function() {
+$(document).ready(function() {
 	q = $.jStorage.get('search_query');
    	if($('#search').length) {
 		if (q!=""){
@@ -9,57 +7,106 @@
 			$('.bar-indicator').show();
 			search($('input#page-query').val());
 		}
-	
-	   $('form#search').submit(function() {
+	  $('form#search').submit(function() {
 			$('.bar-indicator').show();
-        	search($('input#page-query').val());
-      		return false;
+       	search($('input#page-query').val());
+     		return false;
 		});
 	} 
 	
 	$('form.navbar-search').submit(function() {
-      query = $('input#query').val();
-			$.jStorage.set('search_query', query);
-			
-			//this is kinda hacky but let's infer the root from the home button
-			var root = $('.nav-link > a')[0].href.replace('/index.html','');
-			
-			document.location.href = root + "/search.html";
-			return false;
-		});
-  });
-
-  function search(query) {
-  	var relativeRoot = $('.nav-link > a')[0].pathname.replace('/index.html','');
-  	var simulatedSearchUrl = relativeRoot + '/search.html?q=' + encodeURIComponent(query);
-	ga('send', {
-		'hitType': 'pageview',
-		'page': simulatedSearchUrl,
-		'title': 'Search: ' + query
+    query = $('input#query').val();
+    $.jStorage.set('search_query', query);
+    
+    //this is kinda hacky but let's infer the root from the home button
+    var root = $('.nav-link > a')[0].href.replace('/index.html','');
+    
+    document.location.href = root + "/search.html";
+    return false;
 	});
-  	
-    var result = $.getJSON('https://86xw1.api.searchify.com/v1/indexes/docs/search?q=' + encodeURIComponent(query) + '&fetch=title&snippet=text&len=500&callback=?', function(data) {
-        $('div#results').empty();
-		
-    root = $('#root').val();
-    last_char = root.substr(root.length - 1);
-		if (last_char == "/")
-      root = root.slice(0, -1);
-		
-		if(data.matches == 0) {
-			$('div#results').append('<h2>No Results Found</h2>');
-		} else {
-			$('div#results').append('<h2><span class="badge">' + data.matches + '</span> Results Found</h2><hr/>');
-		}
-		
-		$('.bar-indicator').hide();
-		
-    	$.each(data.results, function(index, result) {
+});
+
+function search(query) {
+  var relativeRoot = $('.nav-link > a')[0].pathname.replace('/index.html','');
+  var simulatedSearchUrl = relativeRoot + '/search.html?q=' + encodeURIComponent(query);
+  ga('send', {
+    'hitType': 'pageview',
+    'page': simulatedSearchUrl,
+    'title': 'Search: ' + query
+  });
+  
+var jqxhr = $.getJSON( "example.json", function() {
+  console.log( "success" );
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+ 
+
+  var result = $.getJSON('https://86xw1.api.searchify.com/v1/indexes/docs/search?q=' + encodeURIComponent(query) + '&fetch=title&snippet=text&len=500&callback=?')
+    .done(function(data){
+      $('div#results').empty();
+
+      root = $('#root').val();
+      last_char = root.substr(root.length - 1);
+      if (last_char == "/")
+        root = root.slice(0, -1);
+      
+      if(data.matches == 0) {
+        $('div#results').append('<h2>No Documentation Results Found</h2>');
+      } else {
+        $('div#results').append('<h2><span class="badge">' + data.matches + '</span> Documentation Results Found</h2><hr/>');
+      }
+    
+      $.each(data.results, function(index, result) {
         $('div#results').append('<div class="result">\
-      			<p><a class="title" href="' + root + result.docid + '">' + result.title + '</a><br/>\
-				<a href="' + root + result.docid + '"\><small>' + root + result.docid + '</small></a><br/>\
-      			' + result.snippet_text + '</p>\
-    		<br/></div>')
-    	});
+            <p><a class="title" href="' + root + result.docid + '">' + result.title + '</a><br/>\
+        <a href="' + root + result.docid + '"\><small>' + root + result.docid + '</small></a><br/>\
+            ' + result.snippet_text + '</p>\
+        <br/></div>')
+      });
+    })
+
+    .fail(function(){
+      $('div#results').empty();
+      $('div#results').append('<h2><div class="alert alert-danger">Documentation Search Failed! Please try again.</div></h2>');
+    })
+    
+    .always(function(){
+      $('#indicator').hide();
     });
-  }
+
+  var kb_result = $.getJSON('https://sendgrid.zendesk.com/api/v2/portal/search.json?query=' + encodeURIComponent(query))
+    .done(function(data){
+      $('div#kb-results').empty();
+      
+      if(data.results.length == 0) {
+        $('div#kb-results').append('<h2>No Knowledge Base Results Found</h2>');
+      } else {
+        $('div#kb-results').append('<h2><span class="badge">' + data.results.length + '</span> Knowledge Base Results Found</h2><hr/>');
+      }
+    
+      $.each(data.results, function(index, result) {
+        $('div#kb-results').append('<div class="result">\
+            <p><a class="title" href="' + result.html_url + '">' + result.title + '</a><br/>\
+        <a href="' + result.html_url + '"\><small>' + result.html_url + '</small></a><br/>\
+            ' + result.body.substring(0, 500) + '</p>\
+        <br/></div>')
+      });
+    })
+
+    .fail(function(){
+      $('div#kb-results').empty();
+      $('div#kb-results').append('<h2><div class="alert alert-danger">Knowledge Base Search Failed! Please try again.</div></h2>');
+    })
+    
+    .always(function(){
+      $('#kb-indicator').hide();
+    });
+}
