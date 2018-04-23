0ede2ed00a15b5fce3294b68e03bd2bf88bf5194
Justin W Hall <justinwhall@me.com>
2018-04-23 15:13:34 -0600

    SubNav WIP

---
+++ src/components/BreadCrumbs/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BreadCrumbs extends Component {
+  render() {
+    return (
+      <div className="breadcrumbs">
+        breadCrumbs
+      </div>
+    );
+  }
+}
+
+export default BreadCrumbs;
---
+++ src/pages/for-developers.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ForDevelopers extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div>for Developers</div>
+      </div>
+    );
+  }
+}
+
+export default ForDevelopers;
