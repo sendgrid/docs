dc54426bc4024de5f1d045410d2d61b99313c1f9
Justin W Hall <justinwhall@me.com>
2018-05-22 14:26:39 -0600

    help center layout styles WIP

---
+++ src/constants/homeCallouts.js
@@ -0,0 +1,22 @@
+const HOME_CALLOUTS = [
+  {
+    BADGE: 'Marketing',
+    TITLE: 'Marketing Campaigns',
+    COPY: 'We\'ll walk you through the steps needed to be an effective email sender.',
+    LINK: '/',
+  },
+  {
+    BADGE: 'Development',
+    TITLE: 'API',
+    COPY: 'Add contacts, create segments, create and send campaigns, view your stats, and so much more.',
+    LINK: '/',
+  },
+  {
+    BADGE: 'Development',
+    TITLE: 'SMTP',
+    COPY: 'Add contacts, create segments, create and send campaigns, view your stats, and so much more.',
+    LINK: '/',
+  },
+];
+
+export default HOME_CALLOUTS;
