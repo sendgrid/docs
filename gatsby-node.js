d2f98ad710f61cecbffa18c200f08a57aea705e5
Kaylyn Sigler <kaylyn.sigler@sendgrid.com>
2018-05-25 16:27:09 -0600

    fixing sloppy spacing I accidentially added

--- gatsby-node.js
+++ gatsby-node.js
@@ -205,11 +205,11 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
 };
 
 // SASS Sourcemaps
- exports.modifyWebpackConfig = ({ config }) => {
+  exports.modifyWebpackConfig = ({ config }) => {
   const newConfig = { ...config };
   const loadersArr = ['style', 'css?sourceMap', 'sass', 'sass?sourceMap'];
   if (config._loaders.sass.config.loaders) {
     newConfig._loaders.sass.config.loaders = loadersArr;
   }
   return newConfig;
- };
+  };
