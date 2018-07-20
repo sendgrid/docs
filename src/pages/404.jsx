368b01883a58bc36b91ab4b278976c63a9fef049
Justin W Hall <justinwhall@me.com>
2018-07-20 09:21:00 -0600

    404 component wip

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div className="containert">
+        <h1>404 not found :(</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
