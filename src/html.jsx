5558ee9ed4e2057623c5a837c73970efa2bf0ab0
richbachman <rb@richbachman.com>
2018-07-30 10:14:50 -0600

    Add sgdotcom favicon. Fix dev callout. Fix link indent on release notes.

--- src/html.jsx
+++ src/html.jsx
@@ -51,6 +51,27 @@ export default class HTML extends React.Component {
           />
           {this.props.headComponents}
           {/* <link rel="shortcut icon" href={favicon} /> */}
+
+          <link rel="shortcut icon" type="image/x-icon" href="https://sendgrid.com/brand/favicons/favicon.ico">
+          <link rel="apple-touch-icon" sizes="57x57" href="https://sendgrid.com/brand/favicons/apple-icon-57x57.png">
+          <link rel="apple-touch-icon" sizes="60x60" href="https://sendgrid.com/brand/favicons/apple-icon-60x60.png">
+          <link rel="apple-touch-icon" sizes="72x72" href="https://sendgrid.com/brand/favicons/apple-icon-72x72.png">
+          <link rel="apple-touch-icon" sizes="76x76" href="https://sendgrid.com/brand/favicons/apple-icon-76x76.png">
+          <link rel="apple-touch-icon" sizes="114x114" href="https://sendgrid.com/brand/favicons/apple-icon-114x114.png">
+          <link rel="apple-touch-icon" sizes="120x120" href="https://sendgrid.com/brand/favicons/apple-icon-120x120.png">
+          <link rel="apple-touch-icon" sizes="144x144" href="https://sendgrid.com/brand/favicons/apple-icon-144x144.png">
+          <link rel="apple-touch-icon" sizes="152x152" href="https://sendgrid.com/brand/favicons/apple-icon-152x152.png">
+          <link rel="apple-touch-icon" sizes="180x180" href="https://sendgrid.com/brand/favicons/apple-icon-180x180.png">
+          <link rel="icon" type="image/png" sizes="192x192" href="https://sendgrid.com/brand/favicons/android-icon-192x192.png">
+          <link rel="icon" type="image/png" sizes="32x32" href="https://sendgrid.com/brand/favicons/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="96x96" href="https://sendgrid.com/brand/favicons/favicon-96x96.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="https://sendgrid.com/brand/favicons/favicon-16x16.png">
+          <link rel="manifest" href="https://sendgrid.com/brand/favicons/manifest.json">
+          <meta name="msapplication-TileColor" content="#ffffff">
+          <meta name="msapplication-TileImage" content="https://sendgrid.com/brand/favicons/ms-icon-144x144.png">
+          <meta name="theme-color" content="#ffffff">
+
+
           {css}
         </head>
         <body>
