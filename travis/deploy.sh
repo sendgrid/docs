f28b9747d78d990706d72907738363eb7d335b74
Justin W Hall <justinwhall@me.com>
2018-08-17 13:14:19 -0600

    Add path to redict config

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,4 +6,6 @@ if [ "$TRAVIS_BRANCH" == "rc-master" ]; then
 else
   S3_BUCKET="s3://sendgrid-knowledge-center-staging"
 fi
+
+AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3api put-bucket-website --bucket sendgrid-knowledge-center-staging --website-configuration file://static/config.json
 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 sync --acl public-read --sse --delete public $S3_BUCKET
