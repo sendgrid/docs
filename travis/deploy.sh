d0e56d1eab84ff8cba63ca2c98b83b42bd84f05f
Justin W Hall <justinwhall@me.com>
2018-08-17 15:43:13 -0600

    Comment out put-bucket-website command

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -7,5 +7,5 @@ else
   S3_BUCKET="s3://sendgrid-knowledge-center-staging"
 fi
 
-AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3api put-bucket-website --bucket sendgrid-knowledge-center-staging --website-configuration file://static/config.json
+#AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3api put-bucket-website --bucket sendgrid-knowledge-center-staging --website-configuration file://static/config.json
 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 sync --acl public-read --sse --delete public $S3_BUCKET
