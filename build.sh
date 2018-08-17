cb1aed6b89311665d7f98a7d52e00b1b9dc810a2
Justin W Hall <justinwhall@me.com>
2018-08-16 20:38:52 -0600

    fix syntax error

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
   yarn run build
-elif [["$TRAVIS_BRANCH" == "master"]]
+elif [["$TRAVIS_BRANCH" == "master"]]; then
   yarn run build --prefix-paths
 fi
