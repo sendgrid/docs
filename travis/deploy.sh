2990e04f9de99a3b7b377cf0747093ea47788df6
Justin W Hall <justinwhall@me.com>
2018-08-20 14:39:17 -0600

    echo build and deploy envs

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,6 +6,6 @@ if [ "$TRAVIS_BRANCH" == "rc-master" ]; then
 else
   S3_BUCKET="s3://sendgrid-knowledge-center-staging"
 fi
-
+echo "Deploying to $S3_BUCKET"
 #AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3api put-bucket-website --bucket sendgrid-knowledge-center-staging --website-configuration file://static/config.json
 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 sync --acl public-read --sse --delete public $S3_BUCKET
--- travis/deploy_open_source.sh
+++ travis/deploy_open_source.sh
@@ -6,4 +6,5 @@ mkdir docs
 mv * docs
 cd ../
 
+echo "Deploying to open source bucket"
 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 sync --acl public-read --sse --delete public s3://sendgrid-knowledge-center-open-source
