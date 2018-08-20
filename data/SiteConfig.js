a2ba77b2aa8a06a6771eab676dc883614cc032bd
Kaylyn Sigler <kaylyn.sigler@sendgrid.com>
2018-08-20 10:53:19 -0600

    updating PR location when you click edit at the bottom of the page

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -5,7 +5,7 @@ module.exports = {
   siteLogo: 'https://sendgrid.com/brand/sg-logo-300.png',
   ogImage: 'https://sendgrid.com/brand/sg-logo-dark-facebook.jpg',
   siteUrl: 'https://sendgrid.com',
-  gitHubMarkdownPath: 'https://github.com/sendgrid/docs/tree/a-new-hope-episode-iv/content/docs',
+  gitHubMarkdownPath: 'https://github.com/sendgrid/docs/tree/develop/content/docs',
   pathPrefix: '/docs',
   siteDescription: 'Learn how to get started using SendGrid. Integrate in minutes, test for free, and get access to documents to help solve your email deliverability problems.',
   siteRss: '/rss.xml',
