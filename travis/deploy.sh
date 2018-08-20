204c2fbcb6edbcc884dbc26417d40f8cbf436728
Justin W Hall <justinwhall@me.com>
2018-08-20 12:00:27 -0600

    build stage

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,7 +4,11 @@
 if [ "$TRAVIS_BRANCH" == "rc-master" ]; then
   S3_BUCKET="s3://sendgrid-knowledge-center-production"
 else
-  S3_BUCKET="s3://sendgrid-knowledge-center-staging"
+  if [ "$OPEN_SOURCE" == "true" ]; then
+    S3_BUCKET="s3://sendgrid-knowledge-center-open-source"
+  else
+    S3_BUCKET="s3://sendgrid-knowledge-center-staging"
+  fi
 fi
 
 #AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3api put-bucket-website --bucket sendgrid-knowledge-center-staging --website-configuration file://static/config.json
