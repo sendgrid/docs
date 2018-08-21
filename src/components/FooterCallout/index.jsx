99c2821337df2ed25c4933bf9baba70fc07d823a
Kaylyn Sigler <kaylyn.sigler@sendgrid.com>
2018-08-21 10:47:43 -0600

    fixing quicklinks

--- src/components/FooterCallout/index.jsx
+++ src/components/FooterCallout/index.jsx
@@ -17,9 +17,9 @@ const FooterCallout = () => (
           <div className="card card--glossary">
             <h3 className="card__title">Quicklinks</h3>
             <p className="glossary-link"><Link to="/for-developers/sending-email/api-getting-started/">Getting Started</Link></p>
-            <p className="glossary-link"><Link to="/api-v3/">API Documentation</Link></p>
+            <p className="glossary-link"><Link to="/api-reference/">API Documentation</Link></p>
             <p className="glossary-link"><Link to="/for-developers/sending-email/libraries/">Code Libraries</Link></p>
-            <p className="glossary-link"><Link to="/help-support/account-and-settings/api-keys/">API Keys</Link></p>
+            <p className="glossary-link"><Link to="/ui/account-and-settings/create-and-manage-api-keys/">API Keys</Link></p>
           </div>
         </div>
       </div>
