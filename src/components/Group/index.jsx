b6ec614c09cd409d1b429a7ddbe36acd978b7efe
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2018-10-20 11:14:40 +0530

    Converted arrow function to normal function
    This is to increase readability

--- src/components/Group/index.jsx
+++ src/components/Group/index.jsx
@@ -1,20 +1,22 @@
 import Link from 'gatsby-link';
 import React from 'react';
 
-const Group = props => (
-  <div className="group-links">
-    {props.edges.map((doc) => {
-      const {
-        permalink,
-        title,
-      } = doc.node.fields;
+function Group(props) {
+  return (
+    <div className="group-links">
+      {props.edges.map((doc) => {
+        const {
+          permalink,
+          title,
+        } = doc.node.fields;
 
-      const docIsVisible = !!doc.node.frontmatter.navigation.show;
+        const docIsVisible = !!doc.node.frontmatter.navigation.show;
 
-      return docIsVisible ? <Link key={permalink} to={permalink} dangerouslySetInnerHTML={{ __html: title }} /> : null;
-    })}
-  </div>
-);
+        return docIsVisible ? <Link key={permalink} to={permalink} dangerouslySetInnerHTML={{ __html: title }} /> : null;
+      })}
+    </div>
+  );
+}
 
 export default Group;
 
