1276bf7730ba8bfd7eb8ab8bea6530e20c55384a
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2018-10-20 11:22:23 +0530

    Converted arrow function to normal function

--- src/components/ReleaseNotes/ReleaseKey.jsx
+++ src/components/ReleaseNotes/ReleaseKey.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 
-const ReleaseKey = () => (
-  <div className="release-notes-key m-bottom-8">
-    <div className="release-notes-key__icon is-size-h5">
-      <div className="icon-ui" />
-      UI Update
+function ReleaseKey() {
+  return(
+    <div className="release-notes-key m-bottom-8">
+      <div className="release-notes-key__icon is-size-h5">
+        <div className="icon-ui" />
+        UI Update
+      </div>
+      <div className="release-notes-key__icon">
+        <div className="icon-api" />
+        API Update
+      </div>
+      <div className="release-notes-key__icon">
+        <div className="icon-partners" />
+        Partners Update
+      </div>
+      <div className="release-notes-key__icon">
+        <div className="icon-price" />
+        Price Update
+      </div>
     </div>
-    <div className="release-notes-key__icon">
-      <div className="icon-api" />
-      API Update
-    </div>
-    <div className="release-notes-key__icon">
-      <div className="icon-partners" />
-      Partners Update
-    </div>
-    <div className="release-notes-key__icon">
-      <div className="icon-price" />
-      Price Update
-    </div>
-  </div>
-);
-
+  );
+}
 
 export default ReleaseKey;
