4649fd1428bf089bb12d9cae4ba53384e8a1b81b
root <root@localhost.localdomain>
2018-10-28 18:17:08 +0530

    demo

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   siteUrl: 'https://sendgrid.com',
   gitHubMarkdownPath: 'https://github.com/sendgrid/docs/tree/develop/content/docs',
   pathPrefix: '/docs',
-  siteDescription: 'Learn how to get started using SendGrid. Integrate in minutes, test for free, and get access to documents to help solve your email deliverability problems.',
+  siteDescription: 'Learn how to get started using SendGrid. Integrate in minutes, free testing, and get access to documents to help solve your email deliverability problems.',
   siteRss: '/rss.xml',
   googleAnalyticsID: 'UA-12399264-1',
   userTwitter: 'sendgrid',
