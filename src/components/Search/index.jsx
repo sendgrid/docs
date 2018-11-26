a195833d50e71f311d7f5ed051a8aa476394fc27
Justin W Hall <justinwhall@me.com>
2018-11-26 10:20:02 -0700

    change hit.url

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -30,6 +30,7 @@ class Search extends Component {
       transformData(hits) {
         const updatedUrlHits = hits.map(((hit) => {
           hit.url = hit.url.replace('#___gatsby', '');
+          hit.url = hit.url.replace('https://sendgrid.com/docs', '');
           if (hit.anchor === '___gatsby') {
             hit.anchor = '';
           }
