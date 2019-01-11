7b25191714d93e7720737f9f8525b4d1211c6e4c
richbachman <rb@richbachman.com>
2019-01-11 16:39:50 -0700

    add basic datalayer gtm tracking.

--- src/componentsMarkdown/AcademyLink.jsx
+++ src/componentsMarkdown/AcademyLink.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import config from '../../data/SiteConfig';
 
+const dataLayer = window.dataLayer || [];
+
+function handleClick() {
+  dataLayer.push({
+    event: 'Event',
+    eventCategory: 'Academy Callout',
+    eventAction: 'Click',
+    eventLabel: 'CTA Click',
+    eventValue: 'test',
+  });
+}
+
 export default function AcademyLink(props) {
   // check for empty AcademyLink
   if (!props.children) {
@@ -11,7 +23,12 @@ export default function AcademyLink(props) {
     <div className="academy-callout">
       <div className="academy-callout__copy">
         {props.children.map(el => el)}
-        <a href={props.courselink}>GO TO COURSE →</a>
+        <a
+          href={props.courselink}
+          onClick={handleClick}
+        >
+          GO TO COURSE →
+        </a>
       </div>
       <div className="academy-callout__img" style={{ backgroundImage: `url(${config.envPrefix + props.img})` }} />
     </div>
