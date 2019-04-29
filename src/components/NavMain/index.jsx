e0760739100d3e0d82d4cf91bf32fc3e783d6ac8
richbachman <rb@richbachman.com>
2019-04-29 16:16:36 -0600

    new nav and footer styles.

--- src/components/NavMain/index.jsx
+++ src/components/NavMain/index.jsx
@@ -32,77 +32,74 @@ class NavMain extends Component {
 
     return (
       <div className="nav-wrap">
-        <div className="nav-secondary">
-          <div className="container-lg">
-            <Link className="nav-secondary__link" to={LINKS.RELEASE_NOTES}>Release Notes</Link>
-            <a className="nav-secondary__link" href={LINKS.SENDGRID}>SendGrid.com</a>
-            <AuthCtx.Consumer>
-              {({ user }) => (
-                user ? (
-                  <div className="nav-secondary__account">
-                    <div className="nav-secondary__name">{user.first_name} {user.last_name} <span className="carret">▾</span></div>
-                    <div className="nav-secondary__account-links">
-                      <a className="nav-secondary__dashboard" href={LINKS.APP}>Dashboard</a>
-                      <a className="nav-secondary__sign-out" href={LINKS.LOGOUT}>Sign Out</a>
-                    </div>
-                  </div>
-                  ) : (
-                    <a className="nav-secondary__link" href={LINKS.APP}>Log In</a>
-                  )
-              )}
-            </AuthCtx.Consumer>
-          </div>
-        </div>
 
-        <div className="nav-main-wrap">
-          <div className="container-lg">
-            <nav className="nav-main">
-              <Link className="nav-main__logo" onClick={this.closeMenu} to="/" >
-                <span className="nav-main__help-center" >Knowledge Center</span>
-              </Link>
+        <nav className="nav-main">
+          <Link className="nav-main__logo" onClick={this.closeMenu} to="/" >
+            <span className="nav-main__help-center" >Knowledge Center</span>
+          </Link>
 
-              <button
-                className={`nav-main__toggle js-menu-toggle ${menuState}`}
-                onClick={this.toggleMenu}
-              >
-                <span className="nav-main__menu" />
-              </button>
+          <button
+            className={`nav-main__toggle js-menu-toggle ${menuState}`}
+            onClick={this.toggleMenu}
+          >
+            <span className="nav-main__menu">Menu</span>
+            <span className="nav-main__menu__closer" />
+          </button>
 
-              <div className={`nav-main__mobile ${menuState}`} >
+          <div className={`nav-main__mobile ${menuState}`} >
 
-                <div className="nav-item">
-                  <a className="nav-main__plain" onClick={this.closeMenu} href={LINKS.SUPPORT}>
-                    Contact Support
-                  </a>
-                </div>
+            <div className="nav-center">
 
-                <div className="nav-item">
-                  <Link className="nav-main__plain" onClick={this.closeMenu} to={LINKS.FOR_DEVELOPERS}>
-                    For Developers
-                  </Link>
-                </div>
-
-                <div className="nav-item">
-                  <a className="nav-main__plain" onClick={this.closeMenu} href={LINKS.STATUS}>
-                    Status
-                  </a>
-                </div>
-
-                <div className="nav-item">
-                  <a className="nav-main__plain" onClick={this.closeMenu} href={LINKS.ACADEMY}>
-                    Academy <span className="tag--beta">New</span>
-                  </a>
-                </div>
-
-                <div className="nav-item nav-item--btn">
-                  <a className="btn nav-main__login" href={LINKS.PRICING}>
-                    Sign Up
-                  </a>
-                </div>
+              <div className="nav-item">
+                <a className="nav-main__plain" onClick={this.closeMenu} href={LINKS.SUPPORT}>
+                  Support
+                </a>
               </div>
-            </nav>
+
+              <div className="nav-item">
+                <Link className="nav-main__plain" onClick={this.closeMenu} to={LINKS.FOR_DEVELOPERS}>
+                  Developers
+                </Link>
+              </div>
+
+              <div className="nav-item">
+                <a className="nav-main__plain" onClick={this.closeMenu} href={LINKS.STATUS}>
+                  Status
+                </a>
+              </div>
+
+              <div className="nav-item">
+                <Link className="nav-main__plain" onClick={this.closeMenu} to={LINKS.RELEASE_NOTES}>
+                  Release Notes
+                </Link>
+              </div>
+
+            </div>
+
+            <div className="nav-right">
+
+              <div className="nav-item">
+                <AuthCtx.Consumer>
+                  {({ user }) => (
+                    user ? (
+                      <a className="nav-secondary__dashboard" href={LINKS.APP}>Dashboard</a>
+                      ) : (
+                        <a className="nav-secondary__link" href={LINKS.APP}>Sign In</a>
+                      )
+                  )}
+                </AuthCtx.Consumer>
+              </div>
+
+              <div className="nav-item nav-item--btn">
+                <a className="nav-main__login" href={LINKS.PRICING}>
+                  Start Free
+                </a>
+              </div>
+
+            </div>
+
           </div>
-        </div>
+        </nav>
       </div>
     );
   }
