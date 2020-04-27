98245bfa36127d299465799ef88267c9476ffbec
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-04-27 12:05:39 -0600

    update dependencies

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import config from '../../../data/SiteConfig';
 
 class SEO extends Component {
   static capitalizeFirstLetter(str) {
-    str = str.split(' ');
+    const strArray = str.split(' ');
 
-    for (let i = 0, x = str.length; i < x; i++) {
-      str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    for (let i = 0, x = str.length; i < x; i += 1) {
+      strArray[i] = strArray[i][0].toUpperCase() + strArray[i].substr(1);
     }
 
-    return str.join(' ');
+    return strArray.join(' ');
   }
 
   render() {
